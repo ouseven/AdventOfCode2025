6e2f052d5ba938d0dabb55bef6811a6435b16578
Krystian Topa <krystian.topa2@gmail.com>
2025-12-09 10:58:54 +0100

    Added Day 9 Part 1

---
+++ AoC9/AoC9_1.py
@@ -0,0 +1,22 @@
+def area(point1,point2):
+    x1,y1=point1[0],point1[1]
+    x2,y2=point2[0],point2[1]
+    return (abs(x1-x2)+1)*(abs(y1-y2)+1)
+
+
+file=open("input.txt")
+
+points=[]
+for line in file:
+    row=line[:-1]
+    row=row.split(',')
+    points.append((int(row[0]),int(row[1])))
+
+edges=[] #list of tuples : distance,point1,point2
+for i in range(len(points)):
+    for j in range(i+1,len(points)):
+        edges.append( (area(points[i],points[j]),points[i],points[j]) )
+edges.sort(reverse=True)
+
+
+print(edges[0])
